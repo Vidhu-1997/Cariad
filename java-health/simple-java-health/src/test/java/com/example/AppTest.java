312989cf84446e9a3cbbb888cf845f3a7fa11773
Vidhu-1997 <Vidhu.uthappa@ltts.com>
2026-03-07 18:20:58 +0530

    updated pom.xml

--- java-health/simple-java-health/src/test/java/com/example/AppTest.java
+++ java-health/simple-java-health/src/test/java/com/example/AppTest.java
@@ -1,11 +1,12 @@
-
 package com.example;
-import org.junit.Test;
-import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
+
     @Test
-    public void sampleTest() {
+    void sampleTest() {
         assertEquals(1, 1);
     }
 }
